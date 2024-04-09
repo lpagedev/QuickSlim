0f9421d95e17e03965f0493ece33e9080d2c85e6
Levi Page <levi@lpage.dev>
2024-04-09 10:13:52 -0700

    Fix Cannot access route context
    Cannot access route context before route finishes, return generic cors.

--- src/Middleware/QuickCorsMiddleware.php
+++ src/Middleware/QuickCorsMiddleware.php
@@ -8,7 +8,6 @@ use Psr\Http\Server\RequestHandlerInterface;
 use QuickSlim\Config\QuickCorsConfig;
 use QuickSlim\Responses\QuickJsonResponse;
 use Slim\Psr7\Request;
-use Slim\Routing\RouteContext;
 
 class QuickCorsMiddleware
 {
@@ -21,19 +20,15 @@ class QuickCorsMiddleware
 
     public function __invoke(Request $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $routeContext = RouteContext::fromRequest($request);
-        $routingResults = $routeContext->getRoutingResults();
-        $methods = $routingResults->getAllowedMethods();
-        $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
-
         // The method options is used for CORS, Cross Origin Resource Sharing, should always return something, just never data.
         // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
         $method = strtolower($request->getMethod());
-        if ($method === 'options') $response = new QuickJsonResponse(''); else
-            $response = $handler->handle($request);
+        if ($method === 'options') $response = new QuickJsonResponse('');
+        else $response = $handler->handle($request);
 
+        $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
         $response = $response->withHeader('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN'] ?? $_SERVER['HTTP_REFERER'] ?? $_SERVER['REMOTE_ADDR']);
-        $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+        $response = $response->withHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
         $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
 
         // Allow requests with Authorization header
